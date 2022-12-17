33b9548ab2c12dac1bc70cce18be41b691d67a1c
LucasLopesFrazao <211031771@aluno.unb.br>
2022-12-17 12:17:46 -0300

    119- Validando CPF de caracteres únicos.
    Co-authored-by: Pedro Henrique <pedrozinho.herique11@gmail.com>

--- src/validators/ValidateCPF.js
+++ src/validators/ValidateCPF.js
@@ -6,6 +6,15 @@ function ValidateCPF(strCPF) {
     strCPF = strCPF.replaceAll(".", "");
     strCPF = strCPF.replaceAll("-", "");
   if (strCPF == "00000000000") return false;
+  if (strCPF == "11111111111") return false;
+  if (strCPF == "22222222222") return false;
+  if (strCPF == "33333333333") return false;
+  if (strCPF == "44444444444") return false;
+  if (strCPF == "55555555555") return false;
+  if (strCPF == "66666666666") return false;
+  if (strCPF == "77777777777") return false;
+  if (strCPF == "88888888888") return false;
+  if (strCPF == "99999999999") return false;
 
   for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
   Resto = (Soma * 10) % 11;
